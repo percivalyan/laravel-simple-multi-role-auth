49b635f1ae9448d787000522ab22d54d1b351dc2
percivalyan <yoryan8888@gmail.com>
2025-05-05 15:39:35 +0700

    (update) make login easier

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,8 +39,8 @@
                 <form action="/login" method="POST">
                     @csrf
                     <div class="input-group mb-3">
-                        <input type="email" name="email" class="form-control" placeholder="Email" required
-                            autofocus />
+                        <input type="email" name="email" class="form-control" placeholder="Email" required autofocus
+                            value="admin@example.com" />
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-envelope"></span>
@@ -48,7 +48,8 @@
                         </div>
                     </div>
                     <div class="input-group mb-3">
-                        <input type="password" name="password" class="form-control" placeholder="Password" required />
+                        <input type="password" name="password" class="form-control" placeholder="Password" required
+                            value="password" />
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-lock"></span>
